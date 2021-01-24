0085d604861c34307dff2d463c1bd7303b4a18ef
Mihaela Orić <oricmihaela@gmail.com>
2021-01-24 20:31:48 +0100

    first commit

---
+++ openCart.js
@@ -0,0 +1,9 @@
+function openCart() {
+    var el = document.getElementById("open");
+    if(el.style.display == "none") {
+        el.style.display = "block";
+    }
+    else {
+        el.style.display = "none";
+    }
+}
